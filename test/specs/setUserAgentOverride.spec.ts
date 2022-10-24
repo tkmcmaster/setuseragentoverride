cbf2481b3b25af1f134aa29d36f778e4a27d77a5
Trevor McMaster <trevor.mcmaster@familysearch.org>
2022-10-24 13:12:12 -0600

    More testing. The top level async causes it to fail

--- test/specs/setUserAgentOverride.spec.ts
+++ test/specs/setUserAgentOverride.spec.ts
@@ -6,12 +6,12 @@ describe("setUserAgentOverride", () => {
     const overrideRequest: CDP.Network.SetUserAgentOverrideRequest = {
       userAgent: "test user agent done"
     };
-    browser.cdp("Network", "setUserAgentOverride", overrideRequest).then(() => {
+    browser.cdp("Network", "setUserAgentOverride", overrideRequest).then(() =>
       browser.cdp("Browser", "getVersion").then((version: CDP.Browser.GetVersionResponse) => {
-        expect(version, "version").to.not.equal(undefined);
+        // Browser.getVersion() works if not in an async block
         expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
-      }).catch((error) => done(error));
-    }).catch((error) => done(error));
+      }).catch((error) => done(error))
+    ).catch((error) => done(error));
   });
 
   it("Network.setUserAgentOverride should work with async", async () => {
@@ -19,8 +19,12 @@ describe("setUserAgentOverride", () => {
       userAgent: "test user agent async"
     };
     await browser.cdp("Network", "setUserAgentOverride", overrideRequest);
+    await browser.url("http://whatsmyuseragent.org/");
+    const text = await browser.$(".intro-text").getText();
+    // The user agent does get updated in an async block
+    expect(text, "text").to.equal(overrideRequest.userAgent);
     const version: CDP.Browser.GetVersionResponse = await browser.cdp("Browser", "getVersion");
-    expect(version, "version").to.not.equal(undefined);
+    // but Browser.getVersion() does not work if in an async block
     expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
   });
 
@@ -28,12 +32,12 @@ describe("setUserAgentOverride", () => {
     const overrideRequest: CDP.Emulation.SetUserAgentOverrideRequest = {
       userAgent: "test user agent done"
     };
-    browser.cdp("Emulation", "setUserAgentOverride", overrideRequest).then(() => {
+    browser.cdp("Emulation", "setUserAgentOverride", overrideRequest).then(() =>
       browser.cdp("Browser", "getVersion").then((version: CDP.Browser.GetVersionResponse) => {
-        expect(version, "version").to.not.equal(undefined);
+        // Browser.getVersion() works if not in an async block
         expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
-      }).catch((error) => done(error));
-    }).catch((error) => done(error));
+      }).catch((error) => done(error))
+    ).catch((error) => done(error));
   });
 
   it("Emulation.setUserAgentOverride should work with async", async () => {
@@ -41,8 +45,51 @@ describe("setUserAgentOverride", () => {
       userAgent: "test user agent async"
     };
     await browser.cdp("Emulation", "setUserAgentOverride", overrideRequest);
+    await browser.url("http://whatsmyuseragent.org/");
+    const text = await browser.$(".intro-text").getText();
+    // The user agent does get updated in an async block
+    expect(text, "text").to.equal(overrideRequest.userAgent);
+    const version: CDP.Browser.GetVersionResponse = await browser.cdp("Browser", "getVersion");
+    // but Browser.getVersion() does not work if in an async block
+    expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
+  });
+
+  it("Network.setUserAgentOverride should work with mixed top async", async () => {
+    const overrideRequest: CDP.Network.SetUserAgentOverrideRequest = {
+      userAgent: "test user agent mixed top async"
+    };
+    await browser.cdp("Network", "setUserAgentOverride", overrideRequest)
+    .then(() => browser.cdp("Browser", "getVersion").then((version: CDP.Browser.GetVersionResponse) => {
+        // The top level is async so this fails
+        expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
+      })
+    );
+  });
+
+  it("Network.setUserAgentOverride should work with mixed async", (done: Mocha.Done) => {
+    const overrideRequest: CDP.Network.SetUserAgentOverrideRequest = {
+      userAgent: "test user agent mixed async"
+    };
+    browser.cdp("Network", "setUserAgentOverride", overrideRequest).then(async () => {
+      const version: CDP.Browser.GetVersionResponse = await browser.cdp("Browser", "getVersion");
+      // The top level is not async so this works
+      expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
+      done();
+    }).catch((error) => done(error));
+  });
+
+  const asyncWrapper = async () => {
+    const overrideRequest: CDP.Emulation.SetUserAgentOverrideRequest = {
+      userAgent: "test user agent async wrapper"
+    };
+    await browser.cdp("Emulation", "setUserAgentOverride", overrideRequest);
     const version: CDP.Browser.GetVersionResponse = await browser.cdp("Browser", "getVersion");
     expect(version, "version").to.not.equal(undefined);
     expect(version.userAgent, "version.userAgent").to.equal(overrideRequest.userAgent);
+  };
+
+  it("Network.setUserAgentOverride should work with async wrapper", (done: Mocha.Done) => {
+    // The top level is not async so this works even though everything downstream is async
+    asyncWrapper().then(() => done()).catch((error) => done(error));
   });
 });
